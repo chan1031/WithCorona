255b57e47a049fc1df666c9601cad011de139284
chan1031 <dragonlsc@naver.com>
2020-11-03 17:11:29 +0900

    DB Giofencing update,Giofencing function

---
+++ app/src/main/java/com/cookandroid/myapplication/gioFencing/GpsService.java
@@ -0,0 +1,142 @@
+package com.example.backgroundserviceexample;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
+import android.widget.Toast;
+
+
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class GpsService extends Service {
+
+    private ArrayList<PointD> mPointList = new ArrayList<PointD>();
+
+    public void addPoint (double x, double y) {
+        mPointList.add(new PointD(x,y));
+    }
+
+    public boolean isPointInPolygon(double x, double y){
+        int size = mPointList.size();
+
+        if (size < 3){
+            return false;
+        }
+
+        int prev = size -1;
+        boolean isInnerPoint = false;
+
+        for (int cur = 0; cur < size; cur++){
+            PointD curPoint = mPointList.get(cur);
+            PointD prevPoint = mPointList.get(prev);
+
+            if (curPoint.getY() < y && prevPoint.getY() >= y || prevPoint.getY() < y && curPoint.getY() >= y){
+                if(curPoint.getX() + (y - curPoint.getY()) / (prevPoint.getY() - curPoint.getY()) * (prevPoint.getX() - curPoint.getX()) < x) {
+                    isInnerPoint =! isInnerPoint;
+                }
+            }
+            prev = cur;
+
+        }
+        return isInnerPoint;
+    }
+
+    final double x1 = 126.680803;
+    final double x2 = 126.682557;
+    final double x3 = 126.681092;
+    final double x4 = 126.679418;
+
+    final double y1 = 37.414736;
+    final double y2 = 37.413644;
+    final double y3 = 37.412501;
+    final double y4 = 37.413728;
+
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        // 지오펜싱 값 지정
+        addPoint(x1,y1);
+        addPoint(x2,y2);
+        addPoint(x3,y3);
+        addPoint(x4,y4);
+
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        final LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        try {
+                lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, // 등록할 위치제공자
+                        10000,
+                        1,
+                        mLocationListener);
+                lm.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, // 등록할 위치제공자
+                        10000,
+                        1,
+                        mLocationListener);
+        } catch (SecurityException ex) {
+        }
+
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    private final LocationListener mLocationListener = new LocationListener() {
+        public void onLocationChanged(Location location) {
+
+            System.out.println("listener test");
+            Log.d("test", "onLocationChanged, location:" + location);
+            double longitude = location.getLongitude();
+            double latitude = location.getLatitude();
+            double altitude = location.getAltitude();
+            float accuracy = location.getAccuracy();
+            String provider = location.getProvider();
+
+            System.out.println( "위도 : " + longitude + "\n경도 : " + latitude
+                    + "\n고도 : " + altitude + "\n정확도 : " + accuracy);
+
+            if(isPointInPolygon(longitude,latitude)){
+                Toast myToast = Toast.makeText(getApplicationContext() ,"In", Toast.LENGTH_SHORT);
+                myToast.show();
+            }else{
+                Toast myToast = Toast.makeText(getApplicationContext(),"out", Toast.LENGTH_SHORT);
+                myToast.show();
+            }
+        }
+
+        public void onProviderDisabled(String provider) {
+            // Disabled시
+            Log.d("test", "onProviderDisabled, provider:" + provider);
+        }
+
+        public void onProviderEnabled(String provider) {
+            // Enabled시
+            Log.d("test", "onProviderEnabled, provider:" + provider);
+        }
+
+        public void onStatusChanged(String provider, int status, Bundle extras) {
+            // 변경시
+            Log.d("test", "onStatusChanged, provider:" + provider + ", status:" + status + " ,Bundle:" + extras);
+        }
+    };
+}
---
+++ app/src/main/java/com/cookandroid/myapplication/gioFencing/PointD.java
@@ -0,0 +1,21 @@
+package com.example.backgroundserviceexample;
+
+// 니가 찾는것이 없으면 직접 만드세요 객체지향두고 뭐하세요?
+public class PointD {
+    double x;
+    double y;
+
+    PointD(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getY(){
+        return y;
+    }
+
+    public double getX(){
+        return x;
+    }
+
+}
